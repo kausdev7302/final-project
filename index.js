8c4a837bb51c9488637857aabef5efcca148424b
kausdev7302 <20cs3025@rgipt.ac.in>
2022-05-22 16:24:54 +0530

    fy

---
+++ index.js
@@ -0,0 +1,53 @@
+bigimage=function(){
+var image=document.querySelector(".one-third-port");
+// p=document.createTextNode("Hello");
+image.innerHTML="this is the image of the ball";
+image.getElementsByClassName("image1")
+}
+normalimage=function(){
+    
+}
+
+
+function f1(){
+    document.getElementById("par1").style.opacity="1"
+}
+function f2(){
+    document.getElementById("par1").style.opacity="0"
+}
+
+
+function f3(){
+    document.getElementById("par2").style.opacity="1"
+}
+function f4(){
+    document.getElementById("par2").style.opacity="0"
+}
+
+
+function f5(){
+    document.getElementById("par3").style.opacity="1"
+}
+function f6(){
+    document.getElementById("par3").style.opacity="0"
+}
+
+function f7(){
+    document.getElementById("par4").style.opacity="1"
+}
+function f8(){
+    document.getElementById("par4").style.opacity="0"
+}
+
+function f9(){
+    document.getElementById("par5").style.opacity="1"
+}
+function f10(){
+    document.getElementById("par5").style.opacity="0"
+}
+function f11(){
+    document.getElementById("par6").style.opacity="1"
+}
+function f12(){
+    document.getElementById("par6").style.opacity="0"
+}
